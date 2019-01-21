8eae9d3f7aed8a745112becc87161cec22a00a9e
GabeHart17 <gdhart@students.chccs.k12.nc.us>
2019-01-21 14:55:35 -0500

    differential drive widget

--- src/main/java/frc/robot/Telemetry.java
+++ src/main/java/frc/robot/Telemetry.java
@@ -4,22 +4,31 @@
 
 package frc.robot;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 import frc.robot.*;
 
 
- public final class Telemetry  {
+ public final class Telemetry {
 
     private static ShuffleboardTab DriveControl = Shuffleboard.getTab("DriveControl");
-    
+    private static NetworkTableEntry left;
+
     public static void init() {
+        Shuffleboard
+        .getTab("DriveControl")
+        .add("Drive Base", RobotMap.diffDrive)
+        .withWidget(BuiltInWidgets.kDifferentialDrive);
         DriveControl.add("test", 17);
-        DriveControl.add("left", 0);
+        left = DriveControl.add("left", 0).getEntry();
     }
 
     public static void update() {
-        DriveControl.add("left", RobotMap.leftController.getSpeed());
+        left.setDouble(RobotMap.leftController.getSpeed());
+
     }
 
  }
